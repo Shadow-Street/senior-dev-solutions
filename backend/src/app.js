dbaa280022e4bdf79092a7249145c9c1193a0d25
jagadeeshan-samsara <jagadeeshan.dharuman@samsara-defi.com>
2026-09-05 09:08:46 +0530

    feat: allow CORS requests from remote IP address

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-  origin: ["http://localhost:8080", "http://localhost:5173", "http://localhost:8081","http://192.168.31.128:5173"],
+  origin: ["http://localhost:8080", "http://localhost:5173", "http://localhost:8081","http://192.168.31.128:5173", "http://98.87.150.151"],
   credentials: true,
 }));
 
